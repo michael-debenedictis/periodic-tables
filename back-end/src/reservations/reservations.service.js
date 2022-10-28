49cb358b49fd54e940ea04f7aafe7e21ebad25b8
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-10-27 21:19:08 -0400

    connecting database

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -5,7 +5,7 @@ async function list(date) {
   if (date) {
     return knex('reservations')
       .select('*')
-      .where({ reservation_date: date })
+      // .where({ reservation_date: date })
   } else {
     return knex('reservations')
       .select('*')
