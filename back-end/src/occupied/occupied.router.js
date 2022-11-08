3dc883e78fb9f41b7ec75b96cee33918e56788c3
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-11-07 22:18:47 -0500

    removed unnecessary files

--- back-end/src/occupied/occupied.router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const controller = require('./occupied.controller');
-const methodNotAllowed = require('../errors/methodNotAllowed');
-
-router.route('/:tableId/seat')
-  .all(methodNotAllowed)
-
-module.exports = router;
